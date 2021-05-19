02da2c9cdd5786798ebf5a3a57aac35dc07c54f5
teddygood <ibear6954@gmail.com>
2021-05-19 12:35:51 +0900

    Add thumbnail image of post

--- django_practice/settings.py
+++ django_practice/settings.py
@@ -135,6 +135,8 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
