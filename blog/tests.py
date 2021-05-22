3da02751e734f90021a293fe88a5e5bd2f19a666
teddygood <ibear6954@gmail.com>
2021-05-22 16:50:28 +0900

    Modulize nav and footer

--- blog/tests.py
+++ blog/tests.py
@@ -8,16 +8,32 @@ class TestView(TestCase):
     def setUp(self):
         self.client = Client()
 
+    def navbar_test(self, soup):
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About', navbar.text)
+
+        logo_btn = navbar.find('a', text='Greed')
+        self.assertEqual(logo_btn.attrs['href'], '/')
+
+        home_btn = navbar.find('a', text='Home')
+        self.assertEqual(home_btn.attrs['href'], '/')
+
+        blog_btn = navbar.find('a', text='Blog')
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')
+
+        about_btn = navbar.find('a', text='About')
+        self.assertEqual(about_btn.attrs['href'], '/about/')
+
     def test_post_list(self):
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
 
         soup = BeautifulSoup(response.content, 'html.parser')
+
         self.assertEqual(soup.title.text, 'Blog')
 
-        navbar = soup.nav
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About', navbar.text)
+        self.navbar_test(soup)
 
         self.assertEqual(Post.objects.count(), 0)
         main_area = soup.find('div', id='main-area')
@@ -55,9 +71,7 @@ class TestView(TestCase):
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        navbar = soup.nav
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About', navbar.text)
+        self.navbar_test(soup)
 
         self.assertIn(post_001.title, soup.title.text)
 
