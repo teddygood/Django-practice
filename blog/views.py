555e3d79ca4b6488b156ae2be09ffbf34244f865
teddygood <ibear6954@gmail.com>
2021-05-24 10:23:23 +0900

    Create category page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post, Category
 
@@ -26,6 +26,27 @@ class PostDetail(DetailView):
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
+
+
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = 'Unclassified'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+            'category': category,
+        }
+    )
+
 # FBV(Function Based View) Style
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')
