109bb26e981fd9c1fcb1b52419cd863be760197a
teddygood <ibear6954@gmail.com>
2021-05-24 09:53:40 +0900

    Add category to the post detail page

--- blog/tests.py
+++ blog/tests.py
@@ -97,26 +97,28 @@ class TestView(TestCase):
         self.assertIn('Nothing', main_area.text)
 
     def test_post_detail(self):
-        post_001 = Post.objects.create(
-            title='first post',
-            content='Hello World',
-            author=self.user_coffee,
-        )
+        # post_001 = Post.objects.create(
+        #     title='first post',
+        #     content='Hello World',
+        #     author=self.user_coffee,
+        # )
 
-        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+        self.assertEqual(self.post_001.get_absolute_url(), '/blog/1/')
 
-        response = self.client.get(post_001.get_absolute_url())
+        response = self.client.get(self.post_001.get_absolute_url())
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
 
         self.navbar_test(soup)
+        self.category_card_test(soup)
 
-        self.assertIn(post_001.title, soup.title.text)
+        self.assertIn(self.post_001.title, soup.title.text)
 
         main_area = soup.find('div', id='main-area')
         post_area = main_area.find('div', id='post-area')
-        self.assertIn(post_001.title, post_area.text)
+        self.assertIn(self.post_001.title, post_area.text)
+        self.assertIn(self.category_programming.name, post_area.text)
 
         self.assertIn(self.user_coffee.username.upper(), post_area.text)
 
-        self.assertIn(post_001.content, post_area.text)
+        self.assertIn(self.post_001.content, post_area.text)
