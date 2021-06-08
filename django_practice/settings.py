f79a38b966ca9c02e830ded6f77be9b5030e0df1
teddygood <ibear6954@gmail.com>
2021-06-08 23:58:04 +0900

    Change the SECRET_KEY

--- django_practice/settings.py
+++ django_practice/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-tn2y_m4y=1+-sl9%lw8fb7#$)1z6r!jif4j9=53la-!sg2#c84')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', 1))
